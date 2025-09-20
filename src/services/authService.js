36391d4cf19afd73fc9229dcabb33bafd7676196
khyathi <khyathijtks@gmail.com>
2025-09-20 09:48:06 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-13-218-147-28.compute-1.amazonaws.com:8081/auth";
+const API_URL = "http://18.212.219.23/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
@@ -15,5 +15,6 @@ export const signup = async (username, email, password) => {
 export const logout = () => {
   localStorage.removeItem("token");
 };
+
 
 
